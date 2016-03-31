f7080bf9c565a07747e1a2f2b0de8e791e8f9604
Jakub Michalak <daran.dasanerjm@gmail.com>
2016-03-31 18:34:25 +0200

    Created class for testing BinarySearch class and implemented first test

---
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -0,0 +1,22 @@
+package edu.iis.mto.bsearch;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.*;
+
+import org.hamcrest.Matchers;
+
+
+import org.junit.Test;
+
+public class BinarySearchTest {
+
+
+	@Test
+	public void whenSequenceLenghtOneAndKeyInSequenceThenResultFound() {
+		int[] sequence = new int[]{3};
+		int key = 3;
+		SearchResult result = BinarySearch.search(key, sequence);
+		assertThat(result.isFound(), equalTo(true));
+	}
+
+}
