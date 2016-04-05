4ea65c246c84470f9fab5401d608fd8e38704d23
Kuba <daran.dasanaer@gmail.com>
2016-04-05 20:28:32 +0200

    fixed sequences for tests to be sorted

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -42,46 +42,46 @@ public class BinarySearchTest {
 	}
 	@Test
 	public void WhenSequenceLengthGreaterThanOneAndSearchedElemenetIsFirstInSequenceThenResultIsFound(){
-		int[] sequence = new int[]{3,5,7,4,2,9};
-		int key = 3;
+		int[] sequence = new int[]{2,3,4,5,7,9};
+		int key = 2;
 		SearchResult result = BinarySearch.search(key, sequence);
 		assertThat(result.isFound(), equalTo(true));
 	}
 	@Test
 	public void WhenSequenceLengthGreaterThanOneAndSearchedElemenetIsFirstInSequenceThenKeyIsFound(){
-		int[] sequence = new int[]{3,5,7,4,2,9};
-		int key = 3;
+		int[] sequence = new int[]{2,3,4,5,7,9};;
+		int key = 2;
 		SearchResult result = BinarySearch.search(key, sequence);
 		assertThat(result.getPosition(), equalTo(0));
 	}
 	@Test
 	public void WhenSequenceLengthGreaterThanOneAndSearchedElemenetIsLastInSequenceThenResultIsFound(){
-		int[] sequence = new int[]{3,5,7,4,2,9};
+		int[] sequence = new int[]{2,3,4,5,7,9};
 		int key = 9;
 		SearchResult result = BinarySearch.search(key, sequence);
 		assertThat(result.isFound(), equalTo(true));
 	}
 	@Test
 	public void WhenSequenceLengthGreaterThanOneAndSearchedElemenetIsLastInSequenceThenKeyIsFound(){
-		int[] sequence = new int[]{3,5,7,4,2,9};
+		int[] sequence = new int[]{2,3,4,5,7,9};
 		int key = 9;
 		SearchResult result = BinarySearch.search(key, sequence);
 		assertThat(result.getPosition(), equalTo(sequence.length-1));
 	}
 	@Test
 	public void WhenSequenceLengthGreaterThanOneAndSearchedElemenetIsMiddleInSequenceThenResultIsFound(){
-		int[] sequence = new int[]{3,5,7,66,4,2,9};
-		int key = 66;
+		int[] sequence = new int[]{2,3,4,5,7,9,66};
+		int key = 5;
 		SearchResult result = BinarySearch.search(key, sequence);
 		assertThat(result.isFound(), equalTo(true));
 	}
 	@Test
 	public void WhenSequenceLengthGreaterThanOneAndSearchedElemenetIsMidlleInSequenceThenKeyIsFound(){
-		int[] sequence = new int[]{3,5,7,66,4,2,9};
-		int key = 66;
+		int[] sequence = new int[]{2,3,4,5,7,9,66};
+		int key = 5;
 		SearchResult result = BinarySearch.search(key, sequence);
 		assertThat(result.getPosition(), equalTo(sequence.length/2));
 	}
-	
+     
 	
 }
